fb347206bb3642722351c26b77452a2a2caa8301
Gabe <Carrollgbusiness@gmail.com>
2026-04-08 16:46:03 -0500

    First Commit

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        /*Use comments and line spacing to visually separate each question.
+Take your time to think of and use meaningful variable names (don't just use num1)
+Print meaningful messages to the screen so that the reader has all of the context
+information for what you are
+         */
+
+        //shows the Larger of two variables
+       int bobSalary = 102000;
+       int garySalary = 185000;
+       int highestSalary = Math.max(bobSalary, garySalary);
+
+       //prints out a message using a string and the "highestSalary" Variable
+        System.out.println("Gary has a higher salary at $" + highestSalary);
+
+        //Find and display the smallest of two variables
+        //Find and display the area of a circle whose radius is 7.25
+        //Find and display the square root a variable after it is set to 5.0
+        //Find and display the distance between the points (5, 10) and (85, 50)
+        //Find and display the absolute (positive) value of a variable after it is set to -3.8
+        //Find and display a random number between 0 and 1
+        //Calculate how many minutes are in 24 days, use a variable for each value
+        //BONUS: How many milliseconds?
+
+
+
+
+    }
+}
